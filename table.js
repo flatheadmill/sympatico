109835ab5307ffbae8973c9d3dceaaadd02b603b
Alan Gutierrez <alan@prettyrobots.com>
2020-02-22 04:12:31 -0600

    Routing table now generates majorities.
    See #3.

--- table.js
+++ table.js
@@ -12,6 +12,15 @@ function sortByLoad (left, right) {
     return compare
 }
 
+
+// Appears that we use our map of particpants and the buckets assigned to them
+// as the definitive, latest table and just keep track of whether that has
+// drifted from the table that is out there getting sorted by the participants.
+//
+// But, somehow I know that having an array of tables is easier to track will
+// make it easier to page this code into programmer memory when I return to it,
+// so there will be a `_tables` array that we'll `pop()` from.
+
 // For the purposes of this implementation I'm going to make a concerted effort
 // to banish the hobgoblin of immutability from my mind. We will construct an
 // object and then initialize it based on whether we are joining or
@@ -22,13 +31,16 @@ class Table {
         this.queue = new Avenue
         this._bucketCount = bucketCount
         this._participants = {}
+        this._order = []
         this._table = []
+        this._tables = []
         this._snapshots = []
     }
 
-    join (participants) {
-        this._participants = participants
-        this._createTable()
+    join (snapshot) {
+        this._participants = snapshot.participants
+        this._order = snapshot.order
+        this.table = this._createTable()
     }
 
     snapshot (promise) {
@@ -40,7 +52,6 @@ class Table {
         for (;;) {
             const max = load.max()
             const min = load.min()
-            console.log(max.buckets.length, min.buckets.length, evenedOut)
             if (max.buckets.length - min.buckets.length == evenedOut) {
                 break
             }
@@ -53,26 +64,44 @@ class Table {
     }
 
     _createTable () {
-        this._table = new Array(this._bucketCount).fill(null)
+        const table = new Array(this._bucketCount).fill(null), order = this._order
         for (const promise in this._participants) {
             const participant = this._participants[promise]
             for (const index of participant.buckets) {
-                assert(this._table[index] == null)
-                this._table[index] = participant.promise
+                assert(table[index] == null)
+                table[index] = participant.promise
             }
+        }
+        switch (order.length) {
+            case 1:
+                return table.map(value => [ value ])
+            case 2:
+                return table.map(value => [ value, order[(order.indexOf(value) + 1) % 2] ])
+            default:
+                return table.map(value => {
+                    const index = order.indexOf(value)
+                    const left = order[(order.length + (index - 1) % order.length) % order.length]
+                    const right = order[(index + 1) % order.length]
+                    return [ value, left, right ]
+                })
         }
     }
 
     arrive (promise, properties) {
         const size = Object.keys(this._participants).length + 1
         if (size == 1) {
+            this._order.push(promise)
             this._participants[promise] = {
                 promise: promise,
                 buckets: new Array(this._bucketCount).fill(0).map((_, index) => index),
                 properties: properties
             }
         } else {
-            this._snapshots[promise] = JSON.parse(JSON.stringify(this._participants))
+            this._snapshots[promise] = JSON.parse(JSON.stringify({
+                participants: this._participants,
+                order: this._order
+            }))
+            this._order.push(promise)
             const load = new RBTree(sortByLoad)
             for (const promise in this._participants) {
                 load.insert(this._participants[promise])
@@ -92,7 +121,7 @@ class Table {
             }
             this._evenOut(load)
         }
-        this._createTable()
+        this._table = this._createTable()
         this.queue.push(JSON.parse(JSON.stringify(this._table)))
     }
 
@@ -104,6 +133,15 @@ class Table {
         delete this._snapshots[promise]
         const participant = this._participants[promise]
         delete this._participants[promise]
+        this._order.splice(this._order.indexOf(promise), 1)
+        const table = JSON.parse(JSON.stringify(this._table))
+        for (const bucket of table) {
+            const index = bucket.indexOf(promise)
+            if (~index) {
+                bucket.splice(index, 1)
+            }
+            assert(bucket.length != 0)
+        }
         const load = new RBTree(sortByLoad)
         for (const promise in this._participants) {
             load.insert(this._participants[promise])
@@ -116,8 +154,8 @@ class Table {
             load.insert(min)
         }
         this._evenOut(load)
-        this._createTable()
-        this.queue.push(this._table)
+        this._tables.push(this._createTable())
+        this.queue.push(table)
     }
 }
 
