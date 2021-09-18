21920eee93cedb8e99f22e3a396eccf6505f666f
Alan Gutierrez <alan@prettyrobots.com>
2021-09-18 01:41:40 -0500

    Implement redistribute as a function.
    Pull it out of the existing table implementation. Make it a unit tested
    building block. Can move it around and maybe into a new home.
    
    Closes #12.

---
+++ redistribute.js
@@ -0,0 +1,39 @@
+// Red-black tree for ordered maps.
+const { RBTree } = require('bintrees')
+
+module.exports = function (comparator, instances, buckets) {
+    buckets = buckets.slice()
+    const counted = []
+    for (let i = 0; i < instances; i++) {
+        counted[i] = 0
+    }
+    for (const index of buckets) {
+        counted[index]++
+    }
+    const load = new RBTree((left, right) => {
+        const compare = counted[left] - counted[right]
+        if (compare == 0) {
+            return left - right
+        }
+        return compare
+    })
+    for (let i = 0; i < instances; i++) {
+        load.insert(i)
+    }
+    const evenedOut = buckets.length % instances == 0 ? 0 : 1
+    for (;;) {
+        const max = load.max()
+        const min = load.min()
+        if (counted[max] - counted[min] == evenedOut) {
+            break
+        }
+        load.remove(max)
+        load.remove(min)
+        counted[min]++
+        counted[max]--
+        buckets[buckets.indexOf(max)] = min
+        load.insert(max)
+        load.insert(min)
+    }
+    return buckets
+}
