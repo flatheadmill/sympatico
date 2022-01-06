e518f6aa76aab2b1ee520361baced06fd80d43f7
Alan Gutierrez <alan@prettyrobots.com>
2022-01-06 00:34:54 -0600

    Implement arrival in Rust.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,14 @@
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
-struct Entry {
+pub struct Entry {
     version: u64,
     node: u32,
     index: u32,
     value: u32,
 }
 
-struct Log {
+pub struct Log {
     minimum: HashMap<u32, u64>,
     entries: Vec<Entry>,
     consumer: VecDeque<Entry>,
@@ -23,12 +23,42 @@ impl Log {
         }
     }
 
+    fn check(&mut self) {
+        let mut min = self.minimum();
+        for (_, value) in &self.minimum {
+            if value < &min {
+                min = *value;
+            }
+        }
+        let mut i = 0;
+        loop {
+            if self.entries[i].version == min {
+                break;
+            }
+            i += 1;
+        }
+        self.entries.splice(..i, []);
+    }
+
+    pub fn minimum(&self) -> u64 {
+        self.entries[0].version
+    }
+
+    pub fn maximum(&self) -> u64 {
+        self.entries[self.entries.len() - 1].version
+    }
+
     pub fn arrive(&mut self, node: u32) {
         self.minimum.insert(node, 0);
     }
 
     pub fn push(&mut self, entry: Entry) {
         self.entries.push(entry);
+    }
+
+    pub fn advance(&mut self, node: u32, version: u64) {
+        self.minimum.insert(node, version);
+        self.check();
     }
 }
 
@@ -42,7 +72,16 @@ mod tests {
     fn it_logs() {
         let mut log = Log::new(VecDeque::new());
         log.arrive(0);
+        assert_eq!(log.minimum(), 0);
         log.push(Entry{ version: 0, node: 0, index: 0, value: 0 });
-        assert_eq!(1, 1);
+        log.arrive(1);
+        log.push(Entry{ version: 1, node: 0, index: 0, value: 1 });
+        log.push(Entry{ version: 1, node: 0, index: 1, value: 2 });
+        log.arrive(2);
+        log.push(Entry{ version: 2, node: 0, index: 0, value: 3 });
+        log.advance(2, 1);
+        log.advance(0, 0);
+        log.advance(1, 0);
+        assert_eq!(log.minimum(), 0);
     }
 }
--- test/redux/log.t.js
+++ test/redux/log.t.js
@@ -3,31 +3,31 @@ require('proof')(5, okay => {
     const entries = []
     const log = new Log(entries)
     log.arrive(0)
-    log.push({ version: 0n, node: 0, index: 0, value: 1 })
+    log.push({ version: 0n, node: 0, index: 0, value: 0 })
     log.arrive(1)
     log.push({ version: 1n, node: 0, index: 0, value: 1 })
-    log.push({ version: 1n, node: 0, index: 1, value: 1 })
+    log.push({ version: 1n, node: 0, index: 1, value: 2 })
     log.arrive(2)
-    log.push({ version: 2n, node: 0, index: 0, value: 1 })
+    log.push({ version: 2n, node: 0, index: 0, value: 3 })
     log.advance(2, 1n)
     log.advance(0, 0n)
     log.advance(1, 0n)
     const replay = []
     log.replay(1n, 0, 0, replay)
     okay(replay, [{
-        version: 1n, node: 0, index: 1, value: 1
+        version: 1n, node: 0, index: 1, value: 2
     }, {
-        version: 2n, node: 0, index: 0, value: 1
+        version: 2n, node: 0, index: 0, value: 3
     }], 'replay')
     okay(log.minimum(), 0n, 'minimum')
     okay(entries.splice(0), [{
-        version: 0n, node: 0, index: 0, value: 1
+        version: 0n, node: 0, index: 0, value: 0
     }, {
         version: 1n, node: 0, index: 0, value: 1
     }, {
-        version: 1n, node: 0, index: 1, value: 1
+        version: 1n, node: 0, index: 1, value: 2
     }, {
-        version: 2n, node: 0, index: 0, value: 1
+        version: 2n, node: 0, index: 0, value: 3
     }], 'push')
     log.advance(0, 1n)
     log.advance(1, 1n)
