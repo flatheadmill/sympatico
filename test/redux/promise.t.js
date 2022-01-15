c587ec5da5212b9872832ded393cee0e83063e4c
Alan Gutierrez <alan@prettyrobots.com>
2022-01-14 23:03:04 -0600

    Unit test of Promise.

--- test/redux/promise.t.js
+++ test/redux/promise.t.js
@@ -1,5 +1,9 @@
-require('proof')(1, okay => {
+require('proof')(4, okay => {
     const Promise = require('../../redux/promise')
     const promise = new Promise(0, { now: () => 0 })
-    okay(promise, 'required')
+
+    okay(promise.create(), [ 0, 0 ], 'create')
+    okay(Promise.compare([ 0, 0 ], [ 0, 0 ]), 0, 'compare equal')
+    okay(Promise.compare([ 1, 0 ], [ 0, 0 ]), 1, 'compare tiemstamp greater than')
+    okay(Promise.compare([ 0, 1 ], [ 0, 0 ]), 1, 'compare id greater than')
 })
