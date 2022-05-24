27e3a69c5d6577a355ce3c73e8d3359e8bc1aa30
Alan Gutierrez <alan@prettyrobots.com>
2022-05-23 22:18:53 -0500

    Service discovery design.

--- test/redux/sympatico.t.js
+++ test/redux/sympatico.t.js
@@ -1,5 +1,11 @@
 require('proof')(1, okay => {
     const Sympatico = require('../../redux/sympatico')
-    const sympatico = new Sympatico(0)
+    const sympatico = new Sympatico({ id: 'alpha', when: 0 })
     okay(sympatico, 'require')
+    okay(sympatico.get(), { id: 'alpha', when: 0 }, 'get')
+    sympatico.candidates([
+        [{ id: 'alpha', when: 0 }],
+        [{ id: 'bravo', when: 1 }],
+        [{ id: 'charlie', when: 3 }]
+    ])
 })
